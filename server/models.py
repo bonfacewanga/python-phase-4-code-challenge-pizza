97bfc8b63ffa28b35c502cde5fb4ad20f6a01525
Tangawizi <bonniewanga34@gmail.com>
2024-08-27 17:01:04 +0300

    Completed assignment

--- server/models.py
+++ server/models.py
@@ -20,12 +20,19 @@ class Restaurant(db.Model, SerializerMixin):
     name = db.Column(db.String)
     address = db.Column(db.String)
 
-    # add relationship
+    restaurant_pizzas = db.relationship('RestaurantPizza', backref='restaurant', cascade="all, delete-orphan")
 
-    # add serialization rules
+    serialize_rules = ('-restaurant_pizzas.restaurant',)
 
     def __repr__(self):
         return f"<Restaurant {self.name}>"
+  
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'address': self.address
+        }
 
 
 class Pizza(db.Model, SerializerMixin):
@@ -35,12 +42,19 @@ class Pizza(db.Model, SerializerMixin):
     name = db.Column(db.String)
     ingredients = db.Column(db.String)
 
-    # add relationship
+    restaurant_pizzas = db.relationship('RestaurantPizza', backref='pizza')
 
-    # add serialization rules
+    serialize_rules = ('-restaurant_pizzas.pizza',)
 
     def __repr__(self):
         return f"<Pizza {self.name}, {self.ingredients}>"
+    
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'ingredients': self.ingredients
+        }
 
 
 class RestaurantPizza(db.Model, SerializerMixin):
@@ -49,11 +63,23 @@ class RestaurantPizza(db.Model, SerializerMixin):
     id = db.Column(db.Integer, primary_key=True)
     price = db.Column(db.Integer, nullable=False)
 
-    # add relationships
+    pizza_id = db.Column(db.Integer, db.ForeignKey('pizzas.id'), nullable=False)
+    restaurant_id = db.Column(db.Integer, db.ForeignKey('restaurants.id'), nullable=False)
 
-    # add serialization rules
-
-    # add validation
+    @validates('price')
+    def validate_price(self, key, value):
+        if not (1 <= value <= 30):
+            raise ValueError('Price must be between 1 and 30')
+        return value
 
     def __repr__(self):
         return f"<RestaurantPizza ${self.price}>"
+    
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'price': self.price,
+            'pizza_id': self.pizza_id,
+            'restaurant_id': self.restaurant_id
+        }
+    
